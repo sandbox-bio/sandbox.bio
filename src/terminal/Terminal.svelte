4bf8afe26f3d4dc3a27096a9c1857ed79540084b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-26 15:41:05 -0700

    Refactor: move xterm.js to separate component

--- src/terminal/Terminal.svelte
+++ src/terminal/Terminal.svelte
@@ -1,90 +1,49 @@
 <script>
 // Imports
 import { onMount } from "svelte";
-import { Terminal } from "xterm";
-import { WebLinksAddon } from "xterm-addon-web-links";
-import { SerializeAddon } from "xterm-addon-serialize";
-import { FitAddon } from "xterm-addon-fit";
-import LocalEchoController from "local-echo";
 import Aioli from "@biowasm/aioli";
-import "xterm/css/xterm.css";
-
-import { ANSI_CLEAR } from "./core";
+import XTerm from "./XTerm.svelte";
 
 
 // =============================================================================
 // State
 // =============================================================================
 
-// Terminal state
+// Xterm.js
+let ready = false;
 let term;
-let termEcho;
-let termLinks;
-let termSerialize;
-let termFit;
 
-// UI
-let divTerminal;
-
-// CLI
-let ready = false;  // If true, ready for user input
-let CLI;            // Aioli object
+// Aioli
+let CLI;
 
 
 // =============================================================================
 // On load
 // =============================================================================
 onMount(async () => {
-	// Initialize terminal
-	term = new Terminal({
-		convertEol: true,
-		cursorBlink: true
-	});
-	term.open(divTerminal);
-
-	// Attach addons
-	term.loadAddon(termEcho = new LocalEchoController());
-	term.loadAddon(termSerialize = new SerializeAddon());  // Can be used to save state
-	term.loadAddon(termLinks = new WebLinksAddon());       // Supports links in the terminal
-	term.loadAddon(termFit = new FitAddon());              // Makes terminal fit HTML element
-
-	// Register handlers
-	term.onKey(handleShortcuts);
-	termEcho.addAutocompleteHandler(handleAutocomplete);
-
 	// Initialize Aioli
 	CLI = await new Aioli(["samtools/1.10", "seqtk/1.2"], { env: "stg", debug: true });
-	// Make Terminal ready for user input
-	termFit.fit();
-	input();
-	term.focus();
+
+	// Now we can allow user input
 	ready = true;
 })
 
 
 // =============================================================================
-// Manage user input
+// Run commands
 // =============================================================================
 
-async function exec(cmd)
+async function exec(cmd, callback)
 {
-	if(!cmd || !cmd.trim())
-		return;
-
-	console.log("Command:", cmd);
-
 	let out = "";
 
 	// -------------------------------------------------------------------------
 	// Support basic coreutils commands
 	// -------------------------------------------------------------------------
 
-	// Basic commands
-	if(cmd == "clear")
-		out = ANSI_CLEAR;
 	// ls
 	// FIXME: what if give options to ls?
-	else if(cmd == "ls" || cmd.startsWith("ls "))
+	if(cmd == "ls" || cmd.startsWith("ls "))
 	{
 		const folder = cmd.trim().split(" ")[1];
 		const output = await CLI.ls(folder || ".");
@@ -106,54 +65,13 @@ async function exec(cmd)
 	}
 
 	term.writeln(out);
-}
 
-// Get user input
-function input()
-{
-	termEcho.read("$ ")
-		.then(exec)
-		.catch(console.error)
-		.finally(input);
-}
-
-
-// =============================================================================
-// xterm.js handlers
-// =============================================================================
-
-// Keyboard shortcuts
-function handleShortcuts(key)
-{
-	// Ctrl + L = Clear terminal
-	if(key.domEvent.ctrlKey && key.domEvent.key == "l")
-		term.write(`${ANSI_CLEAR}$ `);
-
-	// Ctrl + A = Beginning of line
-	if(key.domEvent.ctrlKey && key.domEvent.key == "a")
-		termEcho.setCursor(0);
-
-	// Ctrl + E = End of line
-	if(key.domEvent.ctrlKey && key.domEvent.key == "e")
-		termEcho.setCursor(Infinity);
-}
-
-// Auto-completes common commands
-function handleAutocomplete(index, tokens)
-{
-	const command = tokens[0];
-	console.log(index, tokens);
-
-	// Root autocomplete
-	if(index == 0)
-		return ["samtools", "bedtools2"];
-
-	// Samtools autocomplete. Need `&& tokens[index]`, otherwise results in "samtools samtools"
-	if(index == 1 && command == "samtools" && tokens[index])
-		return ["view", "index", "sort"];
-
-	return [];
+	callback();
 }
 </script>
 
-<div bind:this={divTerminal} style="opacity: { ready ? 1 : 0.6 }"></div>
+<XTerm
+	ready={ready}
+	bind:term={term}
+	on:exec={event => exec(event.detail.cmd, event.detail.callback)}
+/>
---
+++ src/terminal/XTerm.svelte
@@ -0,0 +1,136 @@
+<script>
+import { onMount, createEventDispatcher } from "svelte";
+import { Terminal } from "xterm";
+import { WebLinksAddon } from "xterm-addon-web-links";
+import { SerializeAddon } from "xterm-addon-serialize";
+import { FitAddon } from "xterm-addon-fit";
+import LocalEchoController from "local-echo";
+import "xterm/css/xterm.css";
+
+// Constants
+const ANSI_CLEAR = "\x1bc";
+// Dispatch for sending "exec" messages to parent component
+const dispatch = createEventDispatcher();
+
+
+// =============================================================================
+// State
+// =============================================================================
+
+export let ready = false;  // Whether CLI is ready for user input
+export let term;           // XTerm.js object
+
+let addons;                // XTerm.js add-ons
+let divTerminal;           // HTML element where terminal will be drawn
+
+// Ask for user input once ready
+$: if(ready) input();
+
+
+// =============================================================================
+// Initialization
+// =============================================================================
+
+onMount(() => {
+	// Xterm.js
+term = new Terminal({
+	convertEol: true,
+	cursorBlink: true
+});
+
+// Xterm.js add-ons
+addons = {
+	echo: new LocalEchoController(),  // Echo controller
+	serialize: new SerializeAddon(),  // Can be used to save state
+	links: new WebLinksAddon(),       // Supports links in the terminal
+	fit: new FitAddon()               // Makes terminal fit HTML element
+};
+
+	// Attach addons
+	for(let addonName in addons)
+		term.loadAddon(addons[addonName]);
+
+	// Register handlers
+	term.onKey(handleShortcuts);
+	addons.echo.addAutocompleteHandler(handleAutocomplete);
+
+	// Prepare UI but don't allow input yet
+	term.open(divTerminal);
+	addons.fit.fit();
+});
+
+
+// =============================================================================
+// xterm.js 
+// =============================================================================
+
+// Get user input 
+function input()
+{
+	term.focus();
+	addons.echo.read("$ ")
+		.then(exec)
+		.catch(console.error)
+		.finally(input);
+}
+
+// Execute command
+function exec(cmd)
+{
+	// Input validation
+	if(!(typeof cmd === "string"))
+		return;
+	cmd = cmd.trim();
+
+	// Handle terminal-related commands
+	if(cmd == "clear") {
+		term.write(ANSI_CLEAR);
+		return;
+	}
+
+	dispatch("exec", {
+		cmd: cmd,
+		callback: input
+	});
+}
+
+
+// =============================================================================
+// xterm.js handlers
+// =============================================================================
+
+// Keyboard shortcuts
+function handleShortcuts(key)
+{
+	// Ctrl + L = Clear terminal
+	if(key.domEvent.ctrlKey && key.domEvent.key == "l")
+		term.write(`${ANSI_CLEAR}$ `);
+
+	// Ctrl + A = Beginning of line
+	if(key.domEvent.ctrlKey && key.domEvent.key == "a")
+		addons.echo.setCursor(0);
+
+	// Ctrl + E = End of line
+	if(key.domEvent.ctrlKey && key.domEvent.key == "e")
+		addons.echo.setCursor(Infinity);
+}
+
+// Auto-completes common commands
+function handleAutocomplete(index, tokens)
+{
+	const command = tokens[0];
+	console.log(index, tokens);
+
+	// Root autocomplete
+	if(index == 0)
+		return ["samtools", "bedtools2"];
+
+	// Samtools autocomplete. Need `&& tokens[index]`, otherwise results in "samtools samtools"
+	if(index == 1 && command == "samtools" && tokens[index])
+		return ["view", "index", "sort"];
+
+	return [];
+}
+</script>
+
+<div bind:this={divTerminal} style="opacity: { ready ? 1 : 0.6 }"></div>
