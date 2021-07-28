a10a16a393c46d33afa72b9cb43ba171a18c2daa
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-28 10:04:48 -0700

    Checkpoint

---
+++ tests/integration/test_piping.js
@@ -0,0 +1,16 @@
+// TODO: 
+// * npm run test needs npm run dev?
+// * how to retrieve output of terminal once execute a command?
+
+// const baseUrl = "http://localhost:5000/";
+// describe("Input validation", () => {
+// 	it("Empty constructor", () => {
+// 		cy.intercept("GET", "/v2/bedtools2/2.29.2/bedtools2.wasm").as("yes")
+// 		cy.visit(baseUrl);
+// 		cy.wait("@yes", { timeout: 10e3 }).then(d => {
+// 			console.log("DONE")
+// 			cy.get(".xterm-helper-textarea").type("pwd\n")	
+// 			expect(5).to.equal(5);
+// 		})
+// 	});
+// });
