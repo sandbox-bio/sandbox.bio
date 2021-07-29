81f69e27d77f521cef2e47db6596b001918c8965
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-29 16:51:29 -0700

    Update Step1 to load IGV.js

--- src/tutorials/bedtools-intro/Step1.svelte
+++ src/tutorials/bedtools-intro/Step1.svelte
@@ -1,5 +1,26 @@
 <script>
 import Execute from "../Execute.svelte";
+import IGV from "../IGV.svelte";
+
+let isOpen = false;
+let igvOptions = {
+	// Explicitly specify the reference URLs so that igv.js doesn't try downloading RefSeq genes
+	reference: {
+		id: "hg19",
+		name: "Human (hg19)",
+		fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg19/hg19.fasta",
+		indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg19/hg19.fasta.fai",
+		cytobandURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg19/cytoBand.txt",
+		tracks: []
+	},
+	locus: "chr1:262,932-512,931",
+	tracks: [
+		{ url: "./data/cpg.bed", name: "CpG islands" },
+		{ url: "./data/exons.bed", name: "RefSeq Exons" },
+		{ url: "./data/gwas.bed", name: "GWAS SNPs" },
+		{ url: "./data/hesc.chromHmm.bed", name: "chromHMM Predictions" }
+	]
+};
 </script>
 
 <p>We preloaded data from <a href="https://science.sciencemag.org/content/337/6099/1190" target="_blank">Maurano et al.</a> into your sandbox. Let's take a look at what files we now have:</p>
@@ -7,7 +28,7 @@ import Execute from "../Execute.svelte";
 <p>Type <Execute command={"ls"} /> in the command line.</p>
 
 <p>Your directory contains 7 <code>BED</code> files and 1 genome file. Use <code>head</code> to preview these files: <Execute command={"head cpg.bed"} /></p>
-	
+
 <p>Three of these files (those starting with <code>f</code> for "fetal tissue") reflect Dnase I hypersensitivity sites measured in different fetal tissue samples.</p>
 
 <span>
@@ -21,13 +42,7 @@ import Execute from "../Execute.svelte";
 </span>
 
 <p>
-The latter 4 files were extracted from the UCSC Genome Browser's <a href="http://genome.ucsc.edu/cgi-bin/hgTables?command=start" target="_blank">Table Browser</a>.
+In order to have a rough sense of the data, let's load those files into IGV: <button class="btn btn-primary" on:click={() => isOpen = !isOpen}>Launch IGV</button>
 </p>
 
-<p>
-In order to have a rough sense of the data, let's load the `cpg.bed`, `exons.bed`, `gwas.bed`, and `hesc.chromHmm.bed` files into [IGV](http://www.broadinstitute.org/igv/). To do this, launch IGV, then click File->Load from File. Then select the four files. IGV will warn you that you need to create an index for a couple of the files. Just click OK, as these indices are created automatically and speed up the processing for IGV.
-</p>
-
-<p>
-Visualization in IGV or other browsers such as UCSC is a tremendously useful way to make sure that your results make sense to your eye. Conveniently, a subset of bedtools is built-into IGV!
-</p>
+<IGV options={igvOptions} {isOpen} />
