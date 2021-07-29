36f82d28015b0ca7d50e0c3933e94ae8802187f5
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-29 09:28:17 -0700

    Create Tutorial component for showing 1 particular tutorial

--- src/Tutorials.svelte
+++ src/Tutorials.svelte
@@ -1,26 +1,32 @@
 <script>
 import { config } from "../tutorials/config";
+import Tutorial from "./Tutorial.svelte";
 
 export let maxListings = Infinity;  // max number of tutorials to list
 
-let id = new URL(window.location).searchParams.get("id");
-let step = new URL(window.location).searchParams.get("step");
+const id = new URL(window.location).searchParams.get("id");
+const step = new URL(window.location).searchParams.get("step");
+const tutorial = config.tutorials.find(t => t.id == id);
 </script>
 
 <!-- List tutorials if none specified -->
-{#if !id}
+{#if !tutorial}
 	<div class="row mt-5">
 		<h3 class="pb-2">Tutorials</h3>
 	</div>
 	<div class="row align-items-md-stretch">
-		{#each config.tutorials.slice(0, maxListings) as tutorial}
+		{#each config.tutorials.slice(0, maxListings) as info}
 			<div class="col-md-3">
 				<div class="h-100 p-5 border rounded-3">
-					<h4>{tutorial.name}</h4>
-					<p>{@html tutorial.description}</p>
-					<a href="/tutorials?id={tutorial.id}" class="btn btn-outline-dark text-center">Launch</a>
+					<h4>{info.name}</h4>
+					<p>{@html info.description}</p>
+					<a href="/tutorials?id={info.id}" class="btn btn-outline-dark text-center">Launch</a>
 				</div>
 			</div>
 		{/each}
 	</div>
+
+<!-- If want a specific tutorial -->
+{:else}
+	<Tutorial {id} {step} />
 {/if}
