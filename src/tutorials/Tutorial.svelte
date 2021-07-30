d5535176436259dc9494a6f7ba46aa20d9b399f1
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-30 09:30:18 -0700

    Add offcanvas table of contents -- the dropdown becomes unwieldy for large number of lessons

--- src/tutorials/Tutorial.svelte
+++ src/tutorials/Tutorial.svelte
@@ -1,13 +1,18 @@
 <script>
 import { config } from "../config";
 import Terminal from "../terminal/Terminal.svelte";
-import { ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from "sveltestrap";
+import { DropdownItem, Offcanvas } from "sveltestrap";
 
 export let id;
 export let step = 0;
 
+// State
 const tutorial = config.tutorials.find(t => t.id == id);
+const tocToggle = () => tocOpen = !tocOpen;
+let tocOpen = false;
 let stepInfo = {};
+
+// Reactive statements
 $: nextStep(step);
 
 function nextStep(step)
@@ -57,27 +62,11 @@ function nextStep(step)
 				<hr class="border-2 border-top border-secondary" />
 				<div class="row">
 					<div class="col-10">
-						<button type="button" class="btn btn-sm btn-secondary" on:click={() => step--} disabled={step == 0}>&larr; Previous</button>
-						<button class="btn btn-sm btn-primary" on:click={() => step++} disabled={step == tutorial.steps.length - 1}>Next &rarr;</button>
+						<button type="button" class="btn btn-sm" on:click={() => step--} class:btn-primary={step != 0} class:btn-secondary={step == 0} disabled={step == 0}>&larr; Previous</button>
+						<button class="btn btn-sm" on:click={() => step++} class:btn-primary={step != tutorial.steps.length - 1} class:btn-secondary={step == tutorial.steps.length - 1} disabled={step == tutorial.steps.length - 1}>Next &rarr;</button>
 					</div>
-					<div class="col-2 text-end" style="font-size: 80%">
-						<ButtonDropdown size="sm">
-							<DropdownToggle color="light" class="dropup">
-								<span class="badge rounded-pill bg-info dropdown-toggle" data-toggle="dropdown">{step + 1} / {tutorial.steps.length}</span>
-							</DropdownToggle>
-							<DropdownMenu>
-								<DropdownItem header>Lessons</DropdownItem>
-								{#each tutorial.steps as s, i}
-									<DropdownItem on:click={() => step = i}>
-										{#if i == step}
-											<strong>{s.subtitle || s.name}</strong>
-										{:else}
-											{s.subtitle || s.name}
-										{/if}
-									</DropdownItem>
-								{/each}
-							</DropdownMenu>
-						</ButtonDropdown>
+					<div class="col-2 text-end">
+						<span on:click={tocToggle} class="badge rounded-pill bg-info">{step + 1} / {tutorial.steps.length}</span>
 					</div>
 				</div>
 			</div>
@@ -87,6 +76,23 @@ function nextStep(step)
 		</div>
 	</div>
 </div>
+
+<!-- TODO: this throws "Uncaught TypeError: $context is undefined" when click a lesson, but still seems to work -->
+<Offcanvas header="Lessons" placement="end" isOpen={tocOpen} toggle={tocToggle}>
+	<DropdownItem header>Introduction</DropdownItem>
+	{#each tutorial.steps as s, i}
+		{#if s.header}
+			<DropdownItem header><br />{s.name}</DropdownItem>
+		{/if}
+		<DropdownItem on:click={() => step = i}>
+			{#if i == step}
+				&rarr; <strong>{s.subtitle || s.name}</strong>
+			{:else}
+				<span style="visibility:hidden">&rarr;</span> {s.subtitle || s.name}
+			{/if}
+		</DropdownItem>
+	{/each}
+</Offcanvas>
 
 <style>
 #terminal-wrapper {
