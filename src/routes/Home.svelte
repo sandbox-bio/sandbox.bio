1edc611a19614172e91e0c12135a9a7740f991fa
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-30 21:44:55 -0700

    Update landing page + screenshots

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,8 +3,8 @@ import Tutorials from "./Tutorials.svelte";
 </script>
 
 <div class="container-fluid pt-3">
-	<div class="row p-4 pb-0 pe-lg-0 pt-lg-4 bg-light align-items-center rounded-3 border shadow-lg">
-		<div class="col-lg-7 p-3 p-lg-4 pt-lg-3">
+	<div class="row pb-0 pe-lg-0 bg-light align-items-center rounded-3 border shadow-lg">
+		<div class="col-lg-6 p-3 p-lg-4 pt-lg-3">
 			<h1 class="display-6 fw-bold lh-1">Interactive bioinformatics tutorials</h1>
 			<br />
 			<p class="lead">Learn how to use bioinformatics tools right from your browser.<br />Everything runs in a sandbox, so you can experiment all you want.</p>
@@ -12,8 +12,8 @@ import Tutorials from "./Tutorials.svelte";
 				<a href="/tutorials" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Get started &rarr;</a>
 			</div>
 		</div>
-		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-			<img class="rounded-lg-3" src="images/cli.png" alt="" width="520">
+		<div class="col-lg-5 offset-md-1 p-0 overflow-hidden shadow-lg">
+			<img class="rounded-lg-3" src="images/cli.full.png" alt="" width="800">
 		</div>
 	</div>
 </div>
