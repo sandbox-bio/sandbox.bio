50560952a1e99515c1946b3c726fc1b68e497e03
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-04 10:16:10 -0700

    Add extra tutorial but comment it out because it's incomplete

--- src/config.js
+++ src/config.js
@@ -2,11 +2,13 @@ import { readable } from "svelte/store";
 import { config as bedtoolsIntro } from "tutorials/bedtools-intro/config.js";
 import { config as bowtie2Intro } from "tutorials/bowtie2-intro/config.js";
 import { config as samtoolsIntro } from "tutorials/samtools-intro/config.js";
+import { config as hiddenMessage } from "tutorials/hidden-message/config.js";
 
 export const config = readable({
 	tutorials: [
 		bedtoolsIntro,
 		bowtie2Intro,
-		samtoolsIntro
+		samtoolsIntro,
+		// hiddenMessage  // uncomment this + add "bcftools/1.10" to src/Terminal.svelte
 	]
 });
