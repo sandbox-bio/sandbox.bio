d3fcdb9b72c4f73e7ea9d3a3c2535da89c8cfa79
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-08 09:00:32 -0700

    Add "About" modal

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,4 +1,5 @@
 <script>
+import { Modal } from "sveltestrap";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,6 +9,7 @@ import Terminal from "./Terminal.svelte";
 import { config } from "config";
 
 // State
+let aboutIsOpen = false;  // Whether "About" modal is showing or not
 const path = window.location.pathname;
 const intro = `# This playground is for open-ended exploration.
 # For guided tutorials, see https://sandbox.bio/tutorials
@@ -26,7 +28,7 @@ const intro = `# This playground is for open-ended exploration.
 
 <header class="d-flex flex-wrap justify-content-center py-3 border-bottom ms-4 me-4 mb-3">
 	<a href="/" class="d-flex align-items-center mb-md-0 me-md-auto text-dark text-decoration-none">
-		<span class="fs-4">🧬 sandbox.bio</span>
+		<span class="fs-4">🧬 sandbox.bio</span>&nbsp;
 	</a>
 
 	<ul class="nav nav-pills">
@@ -56,6 +58,9 @@ const intro = `# This playground is for open-ended exploration.
 		<li class="nav-item">
 			<a href="/playground" class="nav-link" class:active={path == "/playground"}>Playground</a>
 		</li>
+		<li class="nav-item">
+			<button class="btn btn-link text-decoration-none" style="padding-top:7px" on:click={() => aboutIsOpen = !aboutIsOpen}>About</button>
+		</li>
 	</ul>
 </header>
 
@@ -70,3 +75,22 @@ const intro = `# This playground is for open-ended exploration.
 		</div>
 	{/if}
 </main>
+
+<Modal body scrollable header="About" toggle={() => aboutIsOpen = !aboutIsOpen} isOpen={aboutIsOpen}>
+	<p>Interactive tutorials for exploring bioinformatics command-line tools in a secure sandbox.</p>
+
+	<p class="lead fw-bold mt-4 mb-1">How does it work?</p>
+	<p>We compiled the bioinformatics tools to WebAssembly so they can run in your browser (see our <a href="https://github.com/biowasm/biowasm" target="_blank">biowasm</a> project).</p>
+
+	<p>To better simulate the terminal environment, we implemented Bash features such as piping (<code>|</code>), file redirection (<code>></code>, <code>>></code>), process substitution (<code>&lt;()</code>), conditional commands (<code>&&</code>, <code>||</code>), asynchronous commands (<code>&</code>), variables (<code>abc=123</code>), and autocomplete.</p>
+
+	<p>GNU Coreutils such as <code>ls</code>, <code>cat</code>, <code>grep</code>, <code>head</code>, <code>wc</code>, and <code>echo</code> were also implemented, though keep in mind we don't currently support most flags (to address that in the future, we'll compile coreutils to WebAssembly!).</p>
+
+	<p>All the files you read and write to are stored temporarily in memory on a <a href="https://emscripten.org/docs/api_reference/Filesystem-API.html" target="_blank">virtual file system</a>. The terminal UI is based on <a href="https://github.com/xtermjs/xterm.js/" target="_blank">xterm.js</a>.</p>
+
+	<p class="lead fw-bold mt-4 mb-1">How to contribute</p>
+	<p>If you have feedback, ideas for new tutorials, or if one of your own bioinformatics tutorials could benefit from being interactive, please <a href="mailto:robert.aboukhalil+sandboxbio@gmail.com">reach out</a>! Note that currently, only the C/C++ bioinformatics tools listed <a href="https://github.com/biowasm/biowasm#supported-tools" target="_blank">here</a> are supported.</p>
+
+	<p class="lead fw-bold mt-4 mb-1">Author</p>
+	<p>Built by <a href="https://www.robertaboukhalil.com/" target="_blank">Robert Aboukhalil</a>.</p>
+</Modal>
