8471b24f47e1ab43bd56b2efef89684a41b35978
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-11 18:30:28 -0700

    Also store IP in analytics so we can reconstruct path taken, but hash the IP for privacy

--- workers/ping/index.js
+++ workers/ping/index.js
@@ -12,8 +12,10 @@ async function handleRequest(request) {
 		return new Response("", { status: 404 });
 
 	// Store in KV, where key = <timestamp>:<uuid>
+	// For privacy, don't store IP addresses, only hashes of them
+	const ip = await hash(request.headers.get("CF-Connecting-IP"));
 	const key = `${new Date().getTime()}:${uuidv4()}`;
-	await PING.put(key, "", { metadata: [pingFrom, pingTo] });
+	await PING.put(key, "", { metadata: [ip, pingFrom, pingTo] });
 
 	// Enable CORS
 	return new Response("PONG");
@@ -24,4 +26,14 @@ function uuidv4() {
 	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
 		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
 	);
+}
+
+// MD5 hash (source: https://stackoverflow.com/a/64795218)
+async function hash(message) {
+	const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
+	const hashBuffer = await crypto.subtle.digest("MD5", msgUint8) // hash the message
+	const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
+	const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('') // convert bytes to hex string  
+
+	return hashHex;
 }
