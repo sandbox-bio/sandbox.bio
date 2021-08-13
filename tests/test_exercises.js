3779a32d5e2fe15c2718309c04bb266e63a049fa
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-13 09:48:57 -0700

    Add tests to make sure the exercise validator works as expected

---
+++ tests/test_exercises.js
@@ -0,0 +1,30 @@
+// Test that the exercise functionality works as expected
+
+describe("Test exercise criteria checker", () => {
+	it("ls (folder)", async () => {
+		// Exercise 1
+		cy.visit("/tutorials?id=bedtools-intro&step=14");
+		cy.get("body").get(".spinner-border").should("not.exist");
+
+		// Initially, there should be no checkmarks
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 0);
+
+		// Solve 1/2 of the criteria
+		cy.get(".xterm-screen").type("echo 1 > notexons.bed{enter}");
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 1);
+
+		// Undo
+		cy.get(".xterm-screen").type("rm notexons.bed{enter}");
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 0);
+
+		// Solve 2/2 of the criteria
+		cy.get(".xterm-screen").type("bedtools complement -i exons.bed -g genome.txt > notexons.bed{enter}");
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 2);
+
+		// Undo
+		cy.get(".xterm-screen").type("echo 2 > notexons.bed{enter}");
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 1);
+		cy.get(".xterm-screen").type("rm notexons.bed{enter}");
+		cy.get("#tutorial-wrapper").find(".bi-check-circle-fill").should("have.length", 0);
+	});
+})
