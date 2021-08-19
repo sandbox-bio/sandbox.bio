26353386a25b8234474724a4c50e7ae1489a3b30
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-19 09:38:17 -0700

    Cleanup main script

--- index.js
+++ index.js
@@ -1,77 +1,76 @@
-// // The biowasm CDN is hosted using Cloudflare Worker Sites where CDN files
-// // are stored in a key-value store (Cloudflare Worker KV). For example:
-// //     key=samtools/1.10/samtools.wasm --> value=<samtools.wasm contents>
-// // 
-// // A Cloudflare Worker (a serverless function) is the entry point for
-// // retrieving those files from the key-value store. This index.js file
-// // defines the code for that entry point. The code is mostly using the
-// // Cloudflare Workers Site template, but modified to enable CORS (so that
-// // biowasm modules can be loaded from non-biowasm.com domains!) and to
-// // log basic stats about the number of downloads per module.
+// Entrypoint for sandbox.bio; based on the Cloudflare Workers Site template.
 
+import { Router } from "itty-router";
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+
+
+// =============================================================================
+// Config
+// =============================================================================
+
+const routerAPI = Router({ base: "/api/v1" });
+const CACHE_CONTROL = {
+	browserTTL: 604800,  // 1 week (default: null)
+	edgeTTL: 172800,     // 2 days (default: 2 days)
+	bypassCache: false   // Do not bypass Cloudflare's cache (default: false)
+};
+
+
+// =============================================================================
+// Routes
+// =============================================================================
+
+routerAPI.get('/todos', () => new Response('Todos Index!'));
+routerAPI.get('/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
+routerAPI.post('/todos', async request => {
+	const content = await request.json();
+	return new Response('Creating Todo: ' + JSON.stringify(content));
+})
+routerAPI.all('*', () => new Response('Not Found.', { status: 404 }));
+
+
+// =============================================================================
+// Process requests
+// =============================================================================
 
 addEventListener("fetch", event => {
 	let response = {};
 
 	// Process user request
+	// TODO: Add logging for successes and failures
 	try {
 		const url = new URL(event.request.url);
-		// TODO: Do routing with /app/playground
-		// TODO: Do routing with /app/tutorials/<id>
-
-		// TODO: API
-		if(url.pathname.startsWith("/api/v1/")) {
-			response = new Response("API v1", { status: 200 });
-		} else {
-			// TODO: Differntiate app and home page
-			// if(path.startsWith("/app"))
-			// if(path == "/")
+		if(url.pathname.startsWith("/api/"))
+			response = routerAPI.handle(event.request);
+		else
 			response = handleStaticPage(event);
-		}
 	} catch (e) {
 		response = new Response("Internal Error", { status: 500 });
 	}
-
-	// Log basic stats
-	// TODO:
 
 	// Return result
 	event.respondWith(response);
 })
 
 async function handleStaticPage(event) {
-	// Retrieve KV value given a path
+	// Retrieve KV value given a path, or show 404
 	try {
-		let response = await getAssetFromKV(event, {
-			mapRequestToAsset: handlePrefix(),
-			cacheControl: {
-				browserTTL: 604800,  // 1 week (default: null)
-				edgeTTL: 172800,     // 2 days (default: 2 days)
-				bypassCache: false   // Do not bypass Cloudflare's cache (default: false)
-			}
+		return await getAssetFromKV(event, {
+			mapRequestToAsset: handlePrefix,
+			cacheControl: CACHE_CONTROL
 		});
-
-		// // Enable CORS
-		// response.headers.set("Access-Control-Allow-Origin", "*");
-		return response;
-	// On error, show a 404 error
 	} catch (e) {
 		return new Response("404 Not Found", { status: 404 });
 	}
 }
 
-function handlePrefix() {
-	return request => {
-		// If path looks like a directory append index.html
-		// e.g. If path is /about/ -> /about/index.html
-		const parsedUrl = new URL(request.url);
-		let pathname = parsedUrl.pathname;
-		if (pathname.endsWith("/"))
-			pathname = pathname.concat("index.html");
+function handlePrefix(request) {
+	// If path looks like a directory, append index.html
+	const parsedUrl = new URL(request.url);
+	let pathname = parsedUrl.pathname;
+	if (pathname.endsWith("/"))
+		pathname = pathname.concat("index.html");
 
-		parsedUrl.pathname = pathname;
-		return new Request(parsedUrl.toString(), request);
-	}
+	parsedUrl.pathname = pathname;
+	return new Request(parsedUrl.toString(), request);
 }
-
