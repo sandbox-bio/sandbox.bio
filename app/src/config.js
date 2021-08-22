6a58c9cb4d011d28177d1eeeda363854c5de003d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-21 21:13:10 -0700

    Make configs writable

--- app/src/config.js
+++ app/src/config.js
@@ -1,4 +1,4 @@
-import { readable } from "svelte/store";
+import { writable } from "svelte/store";
 import { config as bedtoolsIntro } from "tutorials/bedtools-intro/config.js";
 import { config as bowtie2Intro } from "tutorials/bowtie2-intro/config.js";
 import { config as samtoolsIntro } from "tutorials/samtools-intro/config.js";
@@ -6,12 +6,16 @@ import { config as dnaSecrets } from "tutorials/dna-secrets/config.js";
 
 const URL_API = `https://${window.location.hostname != "localhost" ? window.location.hostname : "dev.sandbox.bio"}/api/v1`;
 
-export const config = readable({
+export const config = writable({
 	tutorials: [
 		bedtoolsIntro,
 		bowtie2Intro,
 		samtoolsIntro,
 		dnaSecrets
 	],
-	api: URL_API
+	api: URL_API,
+	cli: {
+		user: "guest",
+		hostname: "sandbox"
+	}
 });
