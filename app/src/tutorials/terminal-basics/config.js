977db4e70292f0305c2bd8380244f2761ae26cce
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-24 16:32:58 -0700

    Update lessons

--- app/src/tutorials/terminal-basics/config.js
+++ app/src/tutorials/terminal-basics/config.js
@@ -1,14 +1,13 @@
 // Steps
 import Intro from "./steps/Intro.md";
-// import Step1 from "./steps/Step1.md";
-// import Step2 from "./steps/Step2.md";
-// import Step3 from "./steps/Step3.md";
-// import Step4 from "./steps/Step4.md";
-// import Step5 from "./steps/Step5.md";
-// import Step6 from "./steps/Step6.md";
-// import Step7 from "./steps/Step7.md";
-// import Step8 from "./steps/Step8.md";
-// import Conclusion from "./steps/Conclusion.md";
+import Step1 from "./steps/Step1.md";
+import Step2 from "./steps/Step2.md";
+import Step3 from "./steps/Step3.md";
+import Step4 from "./steps/Step4.md";
+import Step5 from "./steps/Step5.md";
+import Step6 from "./steps/Step6.md";
+import Step7 from "./steps/Step7.md";
+import Conclusion from "./steps/Conclusion.md";
 
 export const config = {
 	id: "terminal-basics",
@@ -18,18 +17,18 @@ export const config = {
 	tools: ["samtools/1.10"],
 	difficulty: ["beginner"],
 	steps: [
-		{ name: "Terminal Basics", component: Intro },
-		// { name: "The data", component: Step1 },
-		// { name: "Samtools help", component: Step2 },
-		// { name: "Samtools utilities", component: Step3, subtitle: "Converting SAM to BAM", header: true },
-		// { name: "Samtools utilities", component: Step4, subtitle: "Sort BAM files" },
-		// { name: "Samtools utilities", component: Step5, subtitle: "Index BAM files" },
-		// { name: "Explore BAM files", component: Step6, subtitle: "Scrutinize alignments", header: true },
-		// { name: "Explore BAM files", component: Step7, subtitle: "Inspect the header" },
-		// { name: "Explore BAM files", component: Step8, subtitle: "Capture the flag" },
-		// { name: "The end", component: Conclusion, header: true }
+		{ name: "Introduction", component: Intro },
+		{ name: "The Basics", component: Step1, subtitle: "Navigate the terminal", header: true },
+		{ name: "The Basics", component: Step2, subtitle: "Preview files" },
+		{ name: "The Basics", component: Step3, subtitle: "Filter files" },
+		{ name: "Pipelines", component: Step4, subtitle: "Pipes", header: true },
+		{ name: "Pipelines", component: Step5, subtitle: "Output to a file" },
+		{ name: "Pipelines", component: Step6, subtitle: "A warning about '<code>&gt;</code>'" },
+		{ name: "Pipelines", component: Step7, subtitle: "Environment variables" },
+		{ name: "The end", component: Conclusion, header: true }
 	],
 	files: [
-		"data/terminal-basics/orders.tsv"
+		"data/terminal-basics/orders.tsv",
+		"data/terminal-basics/ref.fa",
 	],
 };
