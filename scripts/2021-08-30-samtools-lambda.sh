9b2cfa742f97bce0ce039f8cc72a90dc21527045
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-30 13:59:02 -0700

    Add notes on running samtools in AWS Lambda

---
+++ scripts/2021-08-30-samtools-lambda.sh
@@ -0,0 +1,130 @@
+# ------------------------------------------------------------------------------
+# Compile samtools/htslib on EC2 instance
+# ------------------------------------------------------------------------------
+
+ssh -i ~/D
+sudo yum install -y git zlib-devel bzip2-devel lzma liblzma-devel xz-devel libcurl-devel openssl-devel autoconf gcc ncurses-devel
+sudo yum groupinstall "Development Tools"
+git clone https://github.com/samtools/samtools.git
+git clone https://github.com/samtools/htslib.git
+cd htslib
+git checkout 1.13
+git submodule update --init --recursive
+cd ../samtools
+git checkout 1.13
+
+autoheader
+autoconf -Wno-syntax
+./configure
+make
+make install
+
+
+
+# ------------------------------------------------------------------------------
+# Bundle with Exodus
+# TODO: look into installing musl library
+# ------------------------------------------------------------------------------
+
+# Install musl
+cd ~
+git clone git://git.musl-libc.org/musl
+git checkout v1.2.2	
+cd musl
+./configure
+make
+sudo make install
+
+# Install Exodus
+cd ~
+sudo yum install -y pip
+pip install --user exodus-bundler
+exodus --tarball samtools/samtools | tar -zx
+cd exodus/
+
+# ------------------------------------------------------------------------------
+# Create entrypoint
+# ------------------------------------------------------------------------------
+
+# move this below
+chmod 755 function.sh bootstrap
+
+
+
+
+# Original code from https://docs.aws.amazon.com/lambda/latest/dg/runtimes-walkthrough.html
+cat <<'EOF' > bootstrap
+#!/bin/sh
+set -euo pipefail
+source $LAMBDA_TASK_ROOT/"$(echo $_HANDLER | cut -d. -f1).sh"
+while true
+do
+  HEADERS="$(mktemp)"
+  EVENT_DATA=$(curl -sS -LD "$HEADERS" -X GET "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next")
+  REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
+
+  URL_GCP="https://storage.googleapis.com/genomics-public-data/ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data/NA12878/alignment/NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.20121211.bam"
+  URL_AWS="https://1000genomes.s3.amazonaws.com/phase3/data/NA12878/alignment/NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.20121211.bam"
+  URL_AWS_CRAM="https://1000genomes.s3.amazonaws.com/phase3/data/NA12878/alignment/NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.20121211.bam.cram"
+
+  cd /tmp
+  # /var/task/bin/samtools view -H http://labshare.cshl.edu/shares/schatzlab/www-data/ribbon/DRR01684_merged.chr1.bam > /tmp/samtools.out 2>&1
+  # /var/task/bin/samtools idxstats http://labshare.cshl.edu/shares/schatzlab/www-data/ribbon/DRR01684_merged.chr1.bam > /tmp/samtools.out 2>&1
+  # /var/task/bin/samtools view -c http://labshare.cshl.edu/shares/schatzlab/www-data/ribbon/DRR01684_merged.chr1.bam 1:10e6-10.05e6 > /tmp/samtools.out 2>&1
+
+  /var/task/bin/samtools view "$URL_AWS_CRAM"  > /tmp/samtools.out 2>&1
+  curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" --data-binary "@/tmp/samtools.out"
+done
+EOF
+
+zip --symlinks -r9 ../samtools.zip *
+aws lambda update-function-code --function-name samtools --zip-file fileb://../samtools.zip
+time aws lambda invoke --function-name samtools --payload '{"text":"Hello"}' response.txt; cat response.txt
+
+
+
+
+# TODO:
+# x Test 42bp URL
+# x Test http labshare data
+# x Test s3/gcp URLs from 1K genomes
+# x Test CRAM file + header
+# - Test CRAM file subset --> mount EFS for CRAM ref genomes?
+
+
+s3://1000genomes/phase3/data/NA12878/alignment/NA12878.chrom11.ILLUMINA.bwa.CEU.low_coverage.20121211.bam
+
+samtools view -H https://storage.googleapis.com/genomics-public-data/ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data/NA12878/alignment/NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.20121211.bam
+
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Run on local Docker container: didn't work, probably b/c not using same OS!
+# ------------------------------------------------------------------------------
+# cd /src/samtools-lambda
+
+# # Setup
+# apt-get -y install musl musl-dev musl-tools
+# pip install --user exodus-bundler
+# #
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# ./aws/install
+# rm awscliv2.zip
+# rm -rf ./aws/
+
+
+# # 
+# exodus --tarball ./samtools/samtools | tar -zx  # /usr/local/bin/samtools
+# cd exodus/
+
+# # Deploy
+# zip --symlinks -r9 ../samtools.zip *
+# aws lambda update-function-code --function-name samtools --zip-file fileb://../samtools.zip
+
+# # Launch
+# time aws lambda invoke --function-name samtools --payload '{"text":"Hello"}' response.txt; cat response.txt
