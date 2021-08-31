a85226fea578e18e93fdeaea542962f73f5d922b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-31 15:16:19 -0700

    Add preliminary modal for login/signup

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+import { Modal, TabContent, TabPane } from "sveltestrap";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,6 +9,8 @@ import Terminal from "./terminal/Terminal.svelte";
 import Listings from "./components/Listings.svelte";
 import { config } from "./stores/config";
 import { tutorials } from "./stores/tutorials";
+// State
+let loginIsOpen = false;  // Whether "About" modal is showing or not
 
 // Config
 const intro = $config.playground;
@@ -41,6 +44,9 @@ const params = new URL(window.location).searchParams;
 		<li class="nav-item">
 			<a href="/playground" class="nav-link" class:active={path == "/playground"}>Playground</a>
 		</li>
+		<li class="nav-item">
+			<button class="btn btn-link text-decoration-none" on:click={() => loginIsOpen = !loginIsOpen}>Log in</button>
+		</li>
 	</ul>
 </header>
 
@@ -60,3 +66,51 @@ const params = new URL(window.location).searchParams;
 	{/if}
 </main>
 
+<Modal body header="" toggle={() => loginIsOpen = !loginIsOpen} isOpen={loginIsOpen}>
+	<TabContent>
+		<TabPane tabId="login" active>
+			<span slot="tab"><h5>Log in</h5></span>
+
+			<p class="mt-2 mb-2"><small>Log in to save your progress:</small></p>
+
+			<div class="form-floating mb-1">
+				<input type="email" class="form-control rounded-4" id="floatingInput" placeholder="name@example.com">
+				<label for="floatingInput">Email address</label>
+			</div>
+			<div class="form-floating mb-3">
+				<input type="password" class="form-control rounded-4" id="floatingPassword" placeholder="Password">
+				<label for="floatingPassword">Password</label>
+			</div>
+			<button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Login</button>
+
+			<hr class="my-4">
+
+			<h2 class="fs-6 fw-bold mb-3">Or use a third-party</h2>
+			<button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
+				<i class="bi bi-google"></i>
+				Login with Google
+			</button>
+			<button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
+				<i class="bi bi-github"></i>
+				Login with GitHub
+			</button>
+
+		</TabPane>
+		<TabPane tabId="signup" >
+			<span slot="tab"><h5>Sign up</h5></span>
+
+			<p class="mt-2 mb-2"><small>Create an account to save your progress:</small></p>
+
+			<div class="form-floating mb-1">
+				<input type="email" class="form-control rounded-4" id="floatingInput" placeholder="name@example.com">
+				<label for="floatingInput">Email address</label>
+			</div>
+			<div class="form-floating mb-3">
+				<input type="password" class="form-control rounded-4" id="floatingPassword" placeholder="Password">
+				<label for="floatingPassword">Password</label>
+			</div>
+			<button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Create account</button>
+
+		</TabPane>
+	</TabContent>
+</Modal>
