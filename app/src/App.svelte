91cb5a943a0b0564b4e5f54d0cc7128cc0f6d6e6
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-31 16:02:37 -0700

    Checkpoint

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,21 +1,59 @@
 <script>
 import { Modal, TabContent, TabPane } from "sveltestrap";
+import { createClient } from "@supabase/supabase-js";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./routes/Home.svelte";
 import Tutorial from "./tutorials/Tutorial.svelte";
 import Terminal from "./terminal/Terminal.svelte";
+import Login from "./components/Login.svelte";
 import Listings from "./components/Listings.svelte";
 import { config } from "./stores/config";
 import { tutorials } from "./stores/tutorials";
-// State
-let loginIsOpen = false;  // Whether "About" modal is showing or not
 
 // Config
 const intro = $config.playground;
 const path = window.location.pathname;
 const params = new URL(window.location).searchParams;
+const supabase = createClient($config.supabase.url, $config.supabase.publicKey);
+
+// State
+let loginIsOpen = false;          // Whether "About" modal is showing or not
+let userInfo = supabase.auth.user();  // Equals null if user isn't logged in
+let loginError = false;
+let signupError = false;
+
+// -----------------------------------------------------------------------------
+// User auth
+// -----------------------------------------------------------------------------
+async function signup() {
+	const { user, session, error } = await supabase.auth.signUp({
+		email: "robert.aboukhalil@gmail.com",
+		password: "mypassword",
+	});
+
+	console.log(user, session, error)
+}
+
+async function login(credentials) {
+	const { user, session, error } = await supabase.auth.signIn(credentials);
+
+	if(error) {
+		loginError = error.message;
+	} else {
+		loginError = false;
+		loginIsOpen = false;
+		userInfo = user;
+	}
+}
+
+async function logout() {
+	const { error } = await supabase.auth.signOut();
+	console.error(error);
+	if(error == null)
+		userInfo = null;
+}
 </script>
 
 <svelte:head>
@@ -45,7 +83,19 @@ const params = new URL(window.location).searchParams;
 			<a href="/playground" class="nav-link" class:active={path == "/playground"}>Playground</a>
 		</li>
 		<li class="nav-item">
-			<button class="btn btn-link text-decoration-none" on:click={() => loginIsOpen = !loginIsOpen}>Log in</button>
+			{#if userInfo == null}
+				<button class="btn btn-link text-decoration-none" on:click={() => loginIsOpen = !loginIsOpen}>Log in</button>
+			{:else}
+				<div class="flex-shrink-0 dropdown pt-1 ps-3">
+					<a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+						<img src="https://github.com/robertaboukhalil.png" alt="My profile" width="32" height="32" class="rounded-circle">
+					</a>
+					<ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2" style="">
+						<li><button class="dropdown-item disabled btn-sm" type="button">{userInfo.email}</button></li>
+						<li><button class="dropdown-item" type="button" on:click={logout}>Log out</button></li>
+					</ul>
+				</div>
+			{/if}
 		</li>
 	</ul>
 </header>
@@ -66,51 +116,20 @@ const params = new URL(window.location).searchParams;
 	{/if}
 </main>
 
+<!-- Login/Signup modal -->
 <Modal body header="" toggle={() => loginIsOpen = !loginIsOpen} isOpen={loginIsOpen}>
 	<TabContent>
 		<TabPane tabId="login" active>
 			<span slot="tab"><h5>Log in</h5></span>
-
 			<p class="mt-2 mb-2"><small>Log in to save your progress:</small></p>
-
-			<div class="form-floating mb-1">
-				<input type="email" class="form-control rounded-4" id="loginEmail" placeholder="name@example.com">
-				<label for="loginEmail">Email address</label>
-			</div>
-			<div class="form-floating mb-3">
-				<input type="password" class="form-control rounded-4" id="loginPassword" placeholder="Password">
-				<label for="loginPassword">Password</label>
-			</div>
-			<button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Login</button>
-
-			<hr class="my-4">
-
-			<h2 class="fs-6 fw-bold mb-3">Or use a third-party</h2>
-			<button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
-				<i class="bi bi-google"></i>
-				Login with Google
-			</button>
-			<button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
-				<i class="bi bi-github"></i>
-				Login with GitHub
-			</button>
-
+			<Login type="login" error={loginError} on:login={event => login(event.detail)} />
 		</TabPane>
 		<TabPane tabId="signup" >
 			<span slot="tab"><h5>Sign up</h5></span>
-
 			<p class="mt-2 mb-2"><small>Create an account to save your progress:</small></p>
-
-			<div class="form-floating mb-1">
-				<input type="email" class="form-control rounded-4" id="signupEmail" placeholder="name@example.com">
-				<label for="signupEmail">Email address</label>
-			</div>
-			<div class="form-floating mb-3">
-				<input type="password" class="form-control rounded-4" id="signupPassword" placeholder="Password">
-				<label for="signupPassword">Password</label>
-			</div>
-			<button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Create account</button>
-
+			<Login type="signup" error={signupError} on:signup={event => signup(event.detail)} />
 		</TabPane>
 	</TabContent>
 </Modal>
+
+<!-- <button on:click={signup}>sdfsdf</button> -->
