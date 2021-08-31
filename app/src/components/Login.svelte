b8bc3f753454ed2d945d34db961545c5648e29e3
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-31 16:02:27 -0700

    Create Login/Signup form component

---
+++ app/src/components/Login.svelte
@@ -0,0 +1,50 @@
+<script>
+import { createEventDispatcher } from "svelte";
+import { Alert } from "sveltestrap";
+
+export let type = "login";  // Supported: login, signup
+export let error = false;
+export let success = false;
+
+// Form state
+let email = "";
+let password = "";
+const dispatch = createEventDispatcher();
+
+// Log in or sign up
+function run() {
+	dispatch(type, {
+		email, password
+	});
+}
+</script>
+
+<div class="form-floating mb-1">
+	<input type="email" class="form-control rounded-4" id="email" placeholder="name@example.com" bind:value={email}>
+	<label for="email">Email address</label>
+</div>
+
+<div class="form-floating mb-3">
+	<input type="password" class="form-control rounded-4" id="password" placeholder="Password" bind:value={password} on:keypress={e => { if (e.key === "Enter")run(); }}>
+	<label for="password">Password</label>
+</div>
+
+{#if error}
+	<Alert color="danger">
+		{error}
+	</Alert>
+{/if}
+
+{#if success}
+	<Alert color="danger">
+		{success}
+	</Alert>
+{/if}
+
+<button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" on:click={run}>
+	{#if type == "login"}
+		Log in
+	{:else}
+		Create account
+	{/if}
+</button>
