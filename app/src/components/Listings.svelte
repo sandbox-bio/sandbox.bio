5a91a33554143efe7d0e21ceff0388571e2f28af
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-01 10:42:41 -0700

    Show tutorial progress in listings

--- app/src/components/Listings.svelte
+++ app/src/components/Listings.svelte
@@ -1,4 +1,7 @@
 <script>
+import { Icon, Tooltip } from "sveltestrap";
+import { progress } from "./stores/config";
+
 export let items = [];
 export let title = "Tutorials";
 export let max = Infinity;  // max number of tutorials to list
@@ -17,9 +20,38 @@ export let max = Infinity;  // max number of tutorials to list
 				{#each (info.tags || []) as tag}
 					<span class="badge bg-primary me-1 mb-2">{tag}</span>
 				{/each}
-				<h4>{info.name}</h4>
+				<h4>
+					{info.name}
+					{#if info.id in $progress}
+						{#if $progress[info.id].step == (info.steps.length - 1)}
+							<Icon id={`icon-${info.id}`} class="float-end text-primary" name="check-circle" />
+							<Tooltip target={`icon-${info.id}`}>
+								Done!
+							</Tooltip>
+						{:else if $progress[info.id].step && $progress[info.id].step > 0}
+							<a href={`/tutorials?id=${info.id}&step=${$progress[info.id].step}`}>
+								<Icon id={`icon-${info.id}`} class="float-end text-primary" name="hourglass-split" />
+							</a>	
+							<Tooltip target={`icon-${info.id}`}>
+								You're at step {$progress[info.id].step + 1} / {info.steps.length}
+							</Tooltip>
+						{/if}
+					{/if}
+				</h4>
 				<p>{@html info.description}</p>
-				<a href={info.url || `/tutorials?id=${info.id}`} class="btn btn-outline-primary text-center">Launch</a>
+				<!-- "Explore" listings -->
+				{#if info.url}
+					<a href={info.url} class="btn btn-outline-primary text-center" target="_blank">Launch</a>
+				<!-- "Tutorials" listings -->
+				{:else}
+					<a href={`/tutorials?id=${info.id}&step=${info.id in $progress ? $progress[info.id].step : 0}`} class="btn btn-outline-primary text-center">
+						{#if info.id in $progress}
+							Resume
+						{:else}
+							Launch
+						{/if}
+					</a>
+				{/if}
 			</div>
 		</div>
 	{/each}
