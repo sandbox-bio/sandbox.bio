9f362dfa60372d39d23ddc264c48fdeed7214050
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-01 18:33:17 -0700

    Cleanup

--- app/src/stores/config.js
+++ app/src/stores/config.js
@@ -7,8 +7,7 @@ import { createClient } from "@supabase/supabase-js";
 // -----------------------------------------------------------------------------
 
 const hostname = window.location.hostname == "localhost" ? "dev.sandbox.bio" : window.location.hostname;
-const urlAPI = `https://${hostname}/api/v1`;
-const urlSupabase = {
+const urlsSupabase = {
 	"dev.sandbox.bio": {
 		url: "https://bqjvxpdzkembvixymfae.supabase.co",
 		publicKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTMyODIwNCwiZXhwIjoxOTQ0OTA0MjA0fQ.7DzKM4bOGK1t-pPkfSe-2ALxcW5xWwcsaZfbCMWDBbY"
@@ -20,6 +19,19 @@ const urlSupabase = {
 	"prd.sandbox.bio": {
 		url: "",
 		publicKey: ""
+	}
+};
+const _config = {
+	// API
+	api: `https://${hostname}/api/v1`,
+	// Default environment information
+	hostname: "sandbox",
+	playground: `\u001b[0;37m# This playground is for open-ended exploration.\n# For guided tutorials, see https://sandbox.bio/tutorials\n#\n# Example:\n#   samtools view -o test.bam /samtools/examples/toy.sam\n#   samtools index test.bam\n#   ls test.bam.bai\n#   samtools idxstats test.bam  # idxstats uses the .bai file \u001b[0m\n`,
+	// Default environment variables. These are auto-regenerated in Terminal.svelte:input() even if the user deletes them
+	env: {
+		PS1: '\\u@\\h$ ',
+		USER: "guest",
+		HOME: "/shared/data"
 	}
 };
 
@@ -37,31 +49,11 @@ export const progress = writable({});
 export const loginModal = writable(false);
 
 // Supabase client
-export const supabase = readable(createClient(urlSupabase[hostname].url, urlSupabase[hostname].publicKey));
+export const supabase = readable(createClient(urlsSupabase[hostname].url, urlsSupabase[hostname].publicKey));
 export const user = writable(get(supabase).auth.user());
 
 // App settings (read-only)
-export const config = readable({
-	// API
-	api: urlAPI,
-	// Default environment information
-	hostname: "sandbox",
-	// Default environment variables. These are auto-regenerated in Terminal.svelte:input() even if the user deletes them
-	env: {
-		PS1: '\\u@\\h$ ',
-		USER: "guest",
-		HOME: "/shared/data"
-	},
-	playground: `\u001b[0;37m# This playground is for open-ended exploration.
-# For guided tutorials, see https://sandbox.bio/tutorials
-#
-# Example:
-#   samtools view -o test.bam /samtools/examples/toy.sam
-#   samtools index test.bam
-#   ls test.bam.bai
-#   samtools idxstats test.bam  # idxstats uses the .bai file \u001b[0m
-`
-});
+export const config = readable(_config);
 
 // -----------------------------------------------------------------------------
 // On change
