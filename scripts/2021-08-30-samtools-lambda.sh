5f83d61bfd582a21e9d234fb713aaaa7b5e70056
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-10 08:40:39 -0700

    Cleanup

--- scripts/2021-08-30-samtools-lambda.sh
+++ scripts/2021-08-30-samtools-lambda.sh
@@ -58,7 +58,7 @@ export PATH=$PATH:/usr/local/musl/bin/
 cd ~
 sudo yum install -y pip
 pip install --user exodus-bundler
-exodus --tarball samtools/samtools | tar -zx  # to bundle, just do "samtools/samtools jq"
+exodus --tarball samtools/samtools jq | tar -zx
 cd exodus/
 
 
@@ -83,13 +83,12 @@ do
   cd /tmp
   set +u
 
-  echo "just a test" > /tmp/samtools.out
   # time /var/task/bin/samtools view ${argsList[@]} > /tmp/samtools.out 2>&1 || true
-  # /var/task/bin/samtools > /tmp/samtools.out 2>&1 || true
+  time /var/task/bin/samtools > /tmp/samtools.out 2>&1 || true
 
   set -u
-  /var/task/bin/jq --arg RESPONSE "$(cat /tmp/samtools.out)" '{"statusCode":200,"body": $RESPONSE}' <<< '{}' > /tmp/response.json
-  curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" --data "@/tmp/response.json"
+  time /var/task/bin/jq --arg RESPONSE "$(cat /tmp/samtools.out)" '{"statusCode":200,"body": $RESPONSE}' <<< '{}' > /tmp/response.json
+  time curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" --data "@/tmp/response.json"
 done
 EOF
 
