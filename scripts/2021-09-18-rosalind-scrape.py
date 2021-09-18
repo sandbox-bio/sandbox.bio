555514cf045847eddc3e7784752e53997329dae3
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-18 09:33:49 -0700

    Initial script for fetching problem info from Rosalind

---
+++ scripts/2021-09-18-rosalind-scrape.py
@@ -0,0 +1,45 @@
+# pip3 install requests beautifulsoup4
+
+import re
+import time
+import requests
+from bs4 import BeautifulSoup
+
+# ------------------------------------------------------------------------------
+# Get list of problems (Bioinformatics Stronghold)
+# ------------------------------------------------------------------------------
+
+r = requests.get("http://rosalind.info/problems/list-view/")
+soup = BeautifulSoup(r.content, "html.parser")
+soup_problems = soup.find_all("tr")[1:]
+problems = []
+for problem in soup_problems:
+	problems.append({
+		"id": problem.find_all("td")[0].get_text(),
+		"title": problem.find_all("td")[1].find("a").get_text().strip(),
+		"url": "http://rosalind.info" + problem.find_all("td")[1].find("a").get("href")
+	})
+
+# ------------------------------------------------------------------------------
+# Get each problem's content
+# ------------------------------------------------------------------------------
+
+problem = problems[0]
+
+	time.sleep(2)
+	r = requests.get(problem["url"])
+	soup = BeautifulSoup(r.content, "html.parser")
+
+	pb_given = mathjax_to_md(soup.select(".given-return")[0].parent.get_text()).replace('Given: ', '')
+	pb_return = mathjax_to_md(soup.select(".given-return")[1].parent.get_text()).replace('Return: ', '')
+
+
+# ------------------------------------------------------------------------------
+# Utilities
+# ------------------------------------------------------------------------------
+
+def mathjax_to_md(string):
+	for group in re.findall('\$.+?\$', string):
+		string = string.replace(group, "_" + group[1:-1] + "_")
+	string = string.replace('\\leq', '<=')
+	return string
