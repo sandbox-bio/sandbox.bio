1098d91180803122c4c402db15589ba01141f92f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-22 15:25:11 -0700

    Rosalind: Cleanup UI for exercise

--- app/src/tutorials/rosalind/steps/Exercise.svelte
+++ app/src/tutorials/rosalind/steps/Exercise.svelte
@@ -1,45 +1,28 @@
 <script>
 import { Converter } from "showdown";
 import Alert from "./components/Alert.svelte";
-import { tutorial } from "./stores/tutorials";
+
+export let exercise = {};
 
 const converter = new Converter();
-
-$: console.log($tutorial.step);
-
-let problem = {"id": "RNA", "title": "Transcribing DNA into RNA", "url": "http://rosalind.info/problems/rna/", "given": "A DNA string _t_ having length at most 1000 nt.", "return": "The transcribed RNA string of _t_.", "sample_data": "GATGGAACTTGACTACGTAAATT\\n", "sample_output": "GAUGGAACUUGACUACGUAAAUU\\n"};
 </script>
 
 <Alert>
-	Submit your answer on <a href={problem.url} target="_blank">Rosalind</a>
+	Submit your answer on <a href={exercise.url} target="_blank">Rosalind</a>
 </Alert>
 
 <h6>Given:</h6>
-{@html converter.makeHtml(problem.given)}
+{@html converter.makeHtml(exercise.given.trim())}
 
 <h6>Return:</h6>
-{@html converter.makeHtml(problem.return)}
+{@html converter.makeHtml(exercise.return.trim())}
 
 <h6>Sample Input:</h6>
-{@html converter.makeHtml(problem.sample_data)}
+<code class="text-break">
+{@html converter.makeHtml(exercise.sample_data.trim())}
+</code>
 
 <h6>Sample Output:</h6>
-{@html converter.makeHtml(problem.sample_output)}
-
-<!-- ##### Problem
-
-**Given**: {problem.given}
-
-**Return**: The transcribed RNA string of _t_.
-
-##### Sample Dataset
-
-```
-GATGGAACTTGACTACGTAAATT
-```
-
-##### Sample Output
-
-```
-GAUGGAACUUGACUACGUAAAUU
-``` -->
+<code class="text-break">
+{@html converter.makeHtml(exercise.sample_output.trim())}
+</code>
