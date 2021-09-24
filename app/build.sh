6d159e2f99a4c4858fa462725629099d8382f0d8
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-24 15:53:28 -0700

    Map /rosalind?step=X to /tutorials?id=rosalind&step=X

--- app/build.sh
+++ app/build.sh
@@ -4,9 +4,11 @@
 echo "<!-- Build: $(TZ=America/Los_Angeles date); $(git rev-parse HEAD) -->" >> public/index.html
 
 # Create mock routes (only used for local testing; there's a router server-side to handle this)
-mkdir -p public/{tutorials,playground}
-cp public/index.html public/tutorials/index.html
-cp public/index.html public/playground/index.html
+PAGES=("tutorials" "playground" "rosalind")
+for page in ${PAGES[@]}; do
+	mkdir -p "public/$page"
+	cp public/index.html public/$page/index.html
+done
 
 # Copy over data we need to be available via URL for mounting data as URLs, and for IGV.js
 mkdir -p public/data
