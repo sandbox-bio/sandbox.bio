0920ffc9ef2df46c1e6accef6897043a3e843597
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-01-30 12:19:28 -0800

    Cleanup prereqs

--- app/src/tutorials/course-bash/steps/Intro.svelte
+++ app/src/tutorials/course-bash/steps/Intro.svelte
@@ -5,38 +5,39 @@ import { tutorials } from "../../../stores/tutorials";
 import Listings from "./components/Listings.svelte";
 
 const PREREQ_ID = "terminal-basics";
-const PREREQ_TEXT = `Before we get started, it's encouraged (though not required) to finish the <strong>Terminal Basics</strong> tutorial before you proceed with this course. That tutorial will introduce you to important concepts to help you get up to speed.`;
+const PREREQ_STATUS_DONE = "done";
+const PREREQ_STATUS_STARTED = "started";
+const PREREQ_STATUS_UNSEEN = "unseen";
 const tutorial = $tutorials.find(t => t.id === PREREQ_ID);
 
-let isLoading = true;
-let isPrereqDone = false;
-let isPrereqStarted = false;
-let prerequisite = "";
+let prereqStatus = null;  // done, started, unseen
 $: if($status.app) {
 	const step = $progress[PREREQ_ID]?.step;
 	if(step == null || step == 0) {
-		prerequisite = PREREQ_TEXT;
+		prereqStatus = PREREQ_STATUS_UNSEEN;
 	} else if(step === tutorial.steps.length - 1) {
-		prerequisite = `You already completed the prerequisite <strong><a target="_blank" href="/tutorials?id=${PREREQ_ID}">Terminal Basics</a></strong> tutorial, so you're ready to get started with this course!`;
+		prereqStatus = PREREQ_STATUS_DONE;
 	} else {
-		prerequisite = PREREQ_TEXT;
-		// prerequisite = `You completed ${$progress[PREREQ_ID]?.step} / ${tutorial.steps.length - 1} steps of the prerequisite <strong>Terminal Basics</strong> tutorial. ${PREREQ_TEXT}`;
+		prereqStatus = PREREQ_STATUS_STARTED;
 	}
-
-	// 	
-	// {/if}
-
-	isLoading = false;
 }
-
 </script>
-Welcome!
+
+<p>Welcome to the Bash Wizardry course!</p>
 
 <!-- Wait until app / $progress is loaded and ready to be used -->
-{#if isLoading}
-	Loading
+{#if prereqStatus == null}
+	Loading...
 {:else}
-	{@html prerequisite}
+	{#if prereqStatus === PREREQ_STATUS_DONE}
+		<p>You already completed the prerequisite <strong><a target="_blank" href="/tutorials?id={PREREQ_ID}">Terminal Basics</a></strong> tutorial, so you're ready to get started with this course!</p>
+	{:else}
+		<p>Before we get started, it's encouraged (but not required) to finish the <strong>Terminal Basics</strong> tutorial before you proceed with this course. That tutorial will introduce you to important concepts to help you get up to speed.</p>
 
-	<Listings items={[tutorial]} colMd="12" colLg="12" colXxl="12" title={null} showBtn={false} />
+		{#if prereqStatus === PREREQ_STATUS_STARTED}
+			<p>You already completed {$progress[PREREQ_ID]?.step} out of {tutorial.steps.length - 1} steps.</p>
+		{/if}
+
+		<Listings items={[tutorial]} colMd="12" colLg="12" colXxl="12" title={null} />
+	{/if}
 {/if}
