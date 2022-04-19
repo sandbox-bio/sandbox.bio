27c3a686cb50f0fe63a6fe4b252708515cc3454d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-04-18 20:39:42 -0700

    Fastp tutorial: html report

--- app/src/tutorials/fastp-intro/config.js
+++ app/src/tutorials/fastp-intro/config.js
@@ -2,7 +2,7 @@
 import Intro from "./steps/Intro.md";
 import Step1 from "./steps/Step1.md";
 import Step2 from "./steps/Step2.md";
-// import Step3 from "./steps/Step3.md";
+import Step3 from "./steps/Step3.md";
 // import Step4 from "./steps/Step4.md";
 // import Step5 from "./steps/Step5.md";
 // import Step6 from "./steps/Step6.md";
@@ -24,6 +24,7 @@ export const config = {
 		{ name: "DNA sequencing QC", component: Intro },
 		{ name: "The data", component: Step1 },
 		{ name: "QC Reports", subtitle: "A basic report", component: Step2 },
+		{ name: "QC Reports", subtitle: "An HTML report", component: Step3 },
 		// { name: "Variables", subtitle: "Using variables to count sums", component: Step3, header: true },
 		// { name: "Variables", subtitle: "Auto-initialization of variables", component: Step4 },
 		// { name: "Variables", subtitle: "Exercise", component: Exercise2 },
