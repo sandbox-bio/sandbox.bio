505443bbc117575ce07e7f55db2f0e610126e0e8
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-04-19 18:49:46 -0700

    Fastp tutorial: Add steps for the filter options

--- app/src/tutorials/fastp-intro/config.js
+++ app/src/tutorials/fastp-intro/config.js
@@ -4,13 +4,9 @@ import Step1 from "./steps/Step1.md";
 import Step2 from "./steps/Step2.md";
 import Step3 from "./steps/Step3.md";
 import Step4 from "./steps/Step4.md";
-// import Step5 from "./steps/Step5.md";
-// import Step6 from "./steps/Step6.md";
-// import Conclusion from "./steps/Conclusion.md";
-// // Exercises
-// import Exercise1 from "./exercises/Exercise1.md";
-// import Exercise2 from "./exercises/Exercise2.md";
-// import Exercise3 from "./exercises/Exercise3.md";
+import Step5 from "./steps/Step5.md";
+import Step6 from "./steps/Step6.md";
+import Step7 from "./steps/Step7.md";
 
 export const config = {
 	id: "fastp-intro",
@@ -26,13 +22,9 @@ export const config = {
 		{ name: "QC Reports", subtitle: "Basic reports", component: Step2, header: true },
 		{ name: "QC Reports", subtitle: "HTML reports", component: Step3 },
 		{ name: "QC Reports", subtitle: "JSON reports", component: Step4 },
-		// { name: "Variables", subtitle: "Using variables to count sums", component: Step3, header: true },
-		// { name: "Variables", subtitle: "Auto-initialization of variables", component: Step4 },
-		// { name: "Variables", subtitle: "Exercise", component: Exercise2 },
-		// { name: "Arrays", subtitle: "Using arrays to tally burritos", component: Step5, header: true },
-		// { name: "Arrays", subtitle: "Using arrays to tally costs", component: Step6 },
-		// { name: "Arrays", subtitle: "Exercise", component: Exercise3 },
-		// { name: "The end", component: Conclusion, header: true }
+		{ name: "Filter Options", subtitle: "Output filtered data", component: Step5, header: true },
+		{ name: "Filter Options", subtitle: "Filter out short reads", component: Step6 },
+		{ name: "Filter Options", subtitle: "Trim low quality bases", component: Step7 },
 	],
 	files: [
 		"data/fastp-intro/HG004_R1.fastq.gz",
---
+++ app/src/tutorials/fastp-intro/steps/Step5.md
@@ -0,0 +1,9 @@
+<script>
+import Execute from "components/Execute.svelte";
+</script>
+
+So far, we focused on using `fastp` to evaluate data quality. But if we want to use the filtered data in downstream analyses, we need to save the filtered FASTQ files for later processing. To do so, we can use the `--out` parameters:
+
+<Execute command="fastp \ --in1 HG004_R1.fastq.gz \ --in2 HG004_R2.fastq.gz \ --out1 HG004_R1_filtered.fastq.gz \ --out2 HG004_R2_filtered.fastq.gz" />
+
+This creates the original HTML/JSON reports, along with a filtered FASTQ file for each input: <Execute inline command="ls -l" />
