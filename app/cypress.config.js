b0c666af976bcd3ad4a296f1b72e5eb6530ddb1f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-22 07:29:55 -0700

    Upgrade to Cypress v10

---
+++ app/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  defaultCommandTimeout: 40000,
+  video: false,
+  screenshotOnRunFailure: false,
+  port: 11111,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'tests/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+})
