4b27a115961212a8515533f6d1e89f2d8d2ffcf3
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-22 07:18:59 -0700

    Clean up CSS

--- app/src/tutorials/Tutorial.svelte
+++ app/src/tutorials/Tutorial.svelte
@@ -70,7 +70,7 @@ $tutorial.step = step;
 </script>
 
 <div class="container-fluid pb-3 px-0">
-	<div class="d-grid gap-3" style="grid-template-columns: {nbSteps > 0 ? "1fr 2fr" : ""}; height:85vh; max-height:85vh">
+	<div class="d-grid gap-2" style="grid-template-columns: {nbSteps > 0 ? "1fr 2fr" : ""}; height:85vh; max-height:85vh">
 		{#if $tutorial.steps.length > 0}
 			<div class="bg-light border rounded-3 p-2 d-flex align-items-end flex-column">
 				<div id="tutorial-sidebar" class="w-100 p-2 mb-auto" style="max-height:77vh; overflow-y:scroll; overflow-x:hidden">
