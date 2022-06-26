521e10621f5e4fc9edfa555dc46dea9a380e58cc
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-26 11:53:00 -0700

    Cleanup

--- app/src/components/IDE.svelte
+++ app/src/components/IDE.svelte
@@ -1,41 +1,22 @@
 <script>
-import { onMount } from "svelte";
 import { createEventDispatcher } from "svelte";
 import { EditorView, basicSetup } from "codemirror"
 import { EditorState } from "@codemirror/state";
 import { json } from "@codemirror/lang-json";  // json input/output
 import { cpp } from "@codemirror/lang-cpp";  // cmd
 
-export let lang = null;  // json, cpp, null=no syntax highlighting
-export let code = `"sup"`;
+export let lang;  // json, cpp, null=no syntax highlighting
+export let code;
 
 const dispatch = createEventDispatcher();
 let divIDE;
 let editor;
 
-$: if(divIDE && lang) {
-	console.log(editor);
-	initEditor(lang);
-}
+// (Re)initialize editor if the language changes (but not the code)
+$: if(divIDE) initEditor(lang);
 
-// onMount(() => {
-// 	initEditor(lang, code);
-// 	// const data = {"abc": "sdf", "def": "fsd", "ghi": { "a": 123, "b": 456 }};
-// 	// initEditor(json, JSON.stringify(data, null, 2));
-// 	// console.log(editor);
-// });
-
-// function extract() {
-// 	const code = editor.state.doc.toString();
-// 	console.log(code);
-// }
-
-// function toggle() {
-// 	initEditor(cpp, `function abc(){  return 123; }`);
-// }
-
-function initEditor(lang, doc) {
-	// Define editor state
+function initEditor(lang) {
+	// Define basic extensions
 	const extensions = [
 		// Don't need all of basicSetup (https://github.com/codemirror/basic-setup/blob/main/src/codemirror.ts#L47)
 		basicSetup.slice(2),
@@ -44,17 +25,16 @@ function initEditor(lang, doc) {
 			if (v.docChanged)
 				dispatch("update", v.state.doc.toString());
 		})
-	];  
+	];
+
+	// Add syntax highlighting
 	if(lang === "cpp")
 		extensions.push(cpp());
 	else if(lang === "json")
 		extensions.push(json());
-	const state = EditorState.create({
-		doc: doc || code,
-		extensions
-	});
 
-	// Creating editor for the first time
+	// Create or update editor
+	const state = EditorState.create({ doc: code, extensions });
 	if(!editor)
 		editor = new EditorView({ state, parent: divIDE });
 	// Or update existing editor
