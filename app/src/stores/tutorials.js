4c45ab0f98c1a74e444d4d2880a43c0f4fdf9626
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-17 12:14:55 -0700

    Add list of playgrounds (but don't show them yet)

--- app/src/stores/tutorials.js
+++ app/src/stores/tutorials.js
@@ -29,6 +29,40 @@ export const tutorials = readable([
 	viralAmplicon,
 ]);
 
+// Playgrounds
+export const playgrounds = readable([
+	{
+		name: "Command Line",
+		description: "sdfsdf",
+		url: "/playground",
+		tags: ["command line", "terminal", "bash"]
+	},
+	{
+		name: "Awk",
+		description: "Wrangle tab/space-delimited datasets",
+		url: "/playgrounds?id=awk",
+		tags: ["awk"]
+	},
+	{
+		name: "Grep",
+		description: "Search and filtering utility.",
+		url: "/playgrounds?id=grep",
+		tags: ["grep"]
+	},
+	{
+		name: "Sed",
+		description: "Search and replace utility.",
+		url: "/playgrounds?id=sed",
+		tags: ["sed"]
+	},
+	{
+		name: "Jq",
+		description: "Filter and wrangle JSON data.",
+		url: "/playgrounds?id=jq",
+		tags: ["jq", "json"]
+	}
+]);
+
 // Linkouts
 export const explore = readable([
 	{
