ce7f605c5a97c2d0c73926a0ea3f542cfb09e35a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-09 16:09:56 -0700

    Increase test timeout

--- app/cypress.config.js
+++ app/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: false,
-  defaultCommandTimeout: 40000,
+  defaultCommandTimeout: 100000,
   video: false,
   screenshotOnRunFailure: false,
   port: 11111,
