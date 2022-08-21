12b5b7bf476d524018baf97473d49de2297455c5
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-21 13:41:27 -0700

    Add lsd2

--- app/src/terminal/Terminal.svelte
+++ app/src/terminal/Terminal.svelte
@@ -31,6 +31,7 @@ const TOOLS_DEFAULT = [
 	{ loading: "lazy", tool: "gawk", version: "5.1.0", reinit: true },
 	{ loading: "lazy", tool: "grep", version: "3.7", reinit: true  },
 	{ loading: "lazy", tool: "sed", version: "4.8", reinit: true  },
+	{ loading: "lazy", tool: "lsd2", version: "2.3" },
 	...HTSLIB_TOOLS.map(program => ({ program, tool: "htslib", loading: "lazy", version: "1.10", reinit: true })),
 	...COREUTILS.map(program => ({ program, tool: "coreutils", loading: "lazy", version: "8.32", reinit: true }))
 ];
