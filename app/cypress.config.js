ba34de2035005ada367bb1aa6bf902e540a50a9a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-12-22 08:28:51 -0800

    Fix tests

--- app/cypress.config.js
+++ app/cypress.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress")
+const webpackPreprocessor = require("@cypress/webpack-preprocessor")
 
 module.exports = defineConfig({
   fixturesFolder: false,
@@ -8,8 +9,17 @@ module.exports = defineConfig({
   port: 11111,
   fileServerFolder: "public/",
   e2e: {
-    setupNodeEvents(on, config) {},
-    specPattern: 'tests/**/*.cy.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // Custom Webpack settings so we can import man/*.txt files without error when running tests
+      on("file:preprocessor", webpackPreprocessor({
+        webpackOptions: {
+          module: {
+            rules: [{ test: /\.txt$/, use: "raw-loader" }],
+          },
+        }
+      }))
+    },
+    specPattern: "tests/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
 })
