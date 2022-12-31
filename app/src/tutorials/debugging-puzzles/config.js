dee6bd88bc0458ccd116a853612293bab538f751
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-12-31 07:55:41 -0800

    Debugging Puzzles: simplify chrom sort puzzle and make hints more useful

--- app/src/tutorials/debugging-puzzles/config.js
+++ app/src/tutorials/debugging-puzzles/config.js
@@ -1,6 +1,6 @@
 // Steps
 import Intro from "./steps/Intro.md";
-import PuzzleCsvSort from "./steps/PuzzleCsvSort.md";
+import PuzzleTxtSort from "./steps/PuzzleTxtSort.md";
 import PuzzleBedSpaces from "./steps/PuzzleBedSpaces.md";
 import PuzzleSamQuery from "./steps/PuzzleSamQuery.md";
 
@@ -15,14 +15,14 @@ export const config = {
 	difficulty: ["difficult"],
 	steps: [
 		{ name: "Bioinformatics Debugging Puzzles", component: Intro },
-		{ name: "Puzzles", component: PuzzleCsvSort, subtitle: "The CSV file that could not be sorted", header: true },
+		{ name: "Puzzles", component: PuzzleTxtSort, subtitle: "The TXT file that could not be sorted", header: true },
 		{ name: "Puzzles", component: PuzzleBedSpaces, subtitle: "The BED file that could not be merged" },
 		{ name: "Puzzles", component: PuzzleSamQuery, subtitle: "The SAM file that could not be queried" },
 		// { name: "The end", component: Conclusion, subtitle: "In Review", header: true }
 	],
 	files: [
 		"data/debugging-puzzles/exons.bed", // similar to the ones from bedtools-intro but with space instead of tabs
-		"data/debugging-puzzles/chromosomes.csv",
+		"data/debugging-puzzles/chromosomes.txt",
 		"data/debugging-puzzles/alignments.bam",
 	],
 };
