849bc4e7416aabf99939eb9a94f0dab46a62ca49
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-01-14 21:04:45 -0800

    Studio: fix alerts to not include <alert> tag and remove duplicate text + bring back Execute command

--- app/src/components/Studio.svelte
+++ app/src/components/Studio.svelte
@@ -1,8 +1,32 @@
 <script>
 import * as marked from "marked";
-let step = `Tutorial here`;
+import autosize from 'svelte-autosize';
 
-$: stepCleaned = clean(step);
+// -----------------------------------------------------------------------------
+// State
+// -----------------------------------------------------------------------------
+
+// Initial values
+let tutorial = "Tutorial Markdown goes here.";
+let example = `<Alert>This is a **note** about this tutorial</Alert>
+
+Here is a link: [Link Text](https://google.com)
+
+Get a file listing:
+
+<Execute command="ls -lah" />
+
+Here's a long command:
+
+<Execute command="bedtools merge \\ -i exons.bed -o count,collapse \\ -d 90 -c 1,4" />
+
+Or keep this command <Execute command="ls -lah" inline /> inline with the text using the \`inline\` property.
+`;
+
+
+// -----------------------------------------------------------------------------
+// Marked.js settings
+// -----------------------------------------------------------------------------
 
 // Open URLs in new tab
 const renderer = {
@@ -16,17 +40,16 @@ const tagAlert = {
 	name: "alert",
 	level: "block",
 	start(src) { return src.match(/^<Alert>/)?.index; },
-	tokenizer(src, tokens) {
+	tokenizer(src) {
 		const rule = /^<Alert>[\n]*(.*)[\n]*<\/Alert>/;
 		const match = rule.exec(src);
 		if (match) {
 			const token = {
 				type: "alert",
-				raw: match[1],
+				raw: match[0],
 				text: match[1].trim(),
 				tokens: []
 			};
-			console.log("alert", token)
 			this.lexer.inline(token.text, token.tokens);
 			return token;
 		}
@@ -34,7 +57,7 @@ const tagAlert = {
 	renderer(token) {
 		return `
 			<div class="alert alert-primary" role="alert">
-				${token.text}
+				${this.parser.parseInline(token.tokens)}
 			</div>
 		`;
 	}
@@ -45,60 +68,69 @@ const tagExecute = {
 	name: "execute",
 	level: "block",
 	start(src) { return src.match(/^<Execute/)?.index; },
-	tokenizer(src, tokens) {
+	tokenizer(src) {
 		// return false;
 		const rule = /^<Execute command="(.+?)" (inline )?\/>/;
 		const match = rule.exec(src);
-		console.log("MATCHHHH", match)
 		if (match) {
 			const token = {
 				type: "execute",
-				raw: match[1],
+				raw: match[0],
 				text: match[1].trim(),
-				tokens: []
+				tokens: [],
+				isInline: match[2]?.trim()
 			};
-			console.log("token", token)
 			this.lexer.inline(token.text, token.tokens);
 			return token;
 		}
 	},
 	renderer(token) {
-		console.log("TOKEN", token)
-		// return;
-		// 		<kbd on:click={exec}>
-		// 		{@html command.replace(/ \\ /g, " \\ <br />&nbsp;&nbsp;&nbsp;")}
-		// 	</kbd>
-		return `
-	<div class="card bg-dark text-white font-monospace mb-3">
-		<div class="card-body">
-			${token.text}
-		</div>
-	</div>
-		`;
+		// Here we don't parseInline b/c everything inside that component should be raw text
+		const command = token.text.replace(/ \\ /g, " \\ <br />&nbsp;&nbsp;&nbsp;");
+		if(token.isInline) {
+			return `
+				<kbd>${command}</kbd>
+			`;
+		} else {
+			return `
+				<div class="card bg-dark text-white font-monospace mb-3">
+					<div class="card-body">
+						${command}
+					</div>
+				</div>
+			`;
+		}
 	}
 };
 
-marked.use({ renderer, extensions: [ tagAlert,  ] });
-
-function clean(s) {
-
-	return s;
-}
+marked.use({ renderer, extensions: [ tagAlert, tagExecute ] });
 </script>
 
+<!-- Tutorial -->
 <div class="row">
 	<div class="col-6">
 		<h4>Markdown</h4>
-		<span class="input" role="textbox" contenteditable bind:textContent={step} />
-
-		<!-- <textarea class="form-control" style="height: fit-content" bind:value={step}></textarea> -->
+		<textarea use:autosize class="form-control border border-primary" bind:value={tutorial}></textarea>
 	</div>
 	<div class="col-6">
 		<h4>Preview</h4>
-		{@html marked.parse(stepCleaned)}
+		<div class="border border-primary p-2">
+			{@html marked.parse(tutorial)}
+		</div>
 	</div>
 </div>
 
-<div class="row">
-	Use &lt;Alert&gt;Alert goes **here**&lt;/Alert&gt;
+<hr />
+
+<div class="row mt-4">
+	<div class="col-6">
+		<h4>Examples:</h4>
+		<textarea use:autosize class="form-control border border-secondary" bind:value={example}></textarea>
+	</div>
+	<div class="col-6">
+		<h4>Preview</h4>
+		<div class="border border-secondary p-2">
+			{@html marked.parse(example)}
+		</div>
+	</div>
 </div>
