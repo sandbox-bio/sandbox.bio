09c9e048ec0464f7a9320c3ea4322955495f2cc9
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-07-26 16:45:35 -0700

    Small fixes

--- app/src/routes/Community.svelte
+++ app/src/routes/Community.svelte
@@ -5,7 +5,22 @@ import { tutorials } from "./stores/tutorials";
 
 <h3 class="my-3">Community Tutorials</h3>
 
-<h5 class="bg-primary bg-opacity-75 p-2 text-white">French Institute of Bioinformatics</h5>
-<img src="/logo-ifb.svg" alt="Logo of the IFB" width="300" />
+<h5 class="bg-primary bg-opacity-75 p-2">
+    <a href="https://www.france-bioinformatique.fr/en/home/" class="text-white text-decoration-none" target="_blank" rel="noreferrer">
+        French Institute of Bioinformatics
+    </a>
+</h5>
+
+<a href="https://www.france-bioinformatique.fr/en/home/" target="_blank" rel="noreferrer">
+    <img src="/logo-ifb.svg" alt="Logo of the IFB" width="250" />
+</a>
 
 <Listings title={null} items={$tutorials.filter(t => t.id.startsWith("ifb-"))} showUnlisted={true} />
+
+<p class="text-muted small mt-3">
+    IFB tutorials are licensed under a 
+    <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="_blank" rel="noreferrer">
+        CC-BY-SA
+    </a>
+    license.
+</p>
